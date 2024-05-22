9056b5e6e7979ed057abd85573276e0612c88a29
Jiwon Ryu <108641566+U-G-1@users.noreply.github.com>
2024-05-22 18:29:32 +0900

    init

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+
+const app = express();
+app.set('port', process.env.port || 3000);
+
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send(err.message);
+});
+
+app.listen(app.get('port'), ()=>{
+    console.log(app.get('port'),'번 포트에서 대기중');
+});
