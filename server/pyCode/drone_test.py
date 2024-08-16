3bc8b317c2dd136989d7a9d81438ea7e2a742d04
Jiwon Ryu <108641566+U-G-1@users.noreply.github.com>
2024-08-16 16:38:59 +0900

    py

--- server/pyCode/drone_test.py
+++ server/pyCode/drone_test.py
@@ -1,64 +1,42 @@
+#!/usr/bin/env python3
+
 import asyncio
+
 from mavsdk import System
 from mavsdk.offboard import (OffboardError, PositionNedYaw)
 
 async def run():
+
     drone = System()
-    await drone.connect(system_address="udp://:14540")
+    #await drone.connect(system_address="udp://:14540")
+    await drone.connect(system_address="serial///dev/ttyUSB0:921600")
 
     print("Waiting for drone to connect...")
     async for state in drone.core.connection_state():
         if state.is_connected:
-            print("-- Connected to drone!")
+            print(f"Drone discovered!")
             break
 
     print("Waiting for drone to have a global position estimate...")
     async for health in drone.telemetry.health():
-        if health.is_global_position_ok and health.is_home_position_ok:
-            print("-- Global position state is good enough for flying.")
+        if health.is_global_position_ok:
+            print("Global position estimate ok")
             break
-
-    """print("Fetching amsl altitude at home location....")
+    print("Fetching amsl altitude at home location....")
     async for terrain_info in drone.telemetry.home():
         absolute_altitude = terrain_info.absolute_altitude_m
-        break"""
+        break        
 
     print("-- Arming")
     await drone.action.arm()
+
+    await asyncio.sleep(5)
     
-    print("-- Setting initial setpoint")
-    await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))
+    print("-- Disarming the drone")
+    await drone.action.disarm()
 
-    print("-- Starting offboard")
-    try:
-        await drone.offboard.start()
-    except OffboardError as error:
-        print(f"Starting offboard mode failed \
-                with error code: {error._result.result}")
-        print("-- Disarming")
-        await drone.action.disarm()
-        return
-    
-    print("-- Go 0m North, 0m East, -0.8m Down \
-            within local coordinate system")
-    await drone.offboard.set_position_ned(
-            PositionNedYaw(0.0, 0.0, -0.8, 0.0))
-    await asyncio.sleep(10)
+    print("Drone is disarmed and the script is done.")
 
-    print("-- return home\
-          #1 Set the original coordinate to 0 ") # 아예 좌표를 0으로 처리
-    await drone.offboard.set_position_ned(
-            PositionNedYaw(0.0, 0.0, 0.0, 0.0))
-    await asyncio.sleep(15)
-
-    print("-- Stopping offboard")
-    try:
-        await drone.offboard.stop()
-    except OffboardError as error:
-        print(f"Stopping offboard mode failed \
-                with error code: {error._result.result}")
-        
 if __name__ == "__main__":
-    # Run the asyncio loop
-    asyncio.run(run())
-
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
