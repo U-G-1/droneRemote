e7f96c9bd0b3f7385536cf6e5bea213a4dd21131
Jiwon Ryu <ryujiwon11@naver.com>
2024-08-23 16:35:12 +0900

    Update saveLocation.py

--- server/pyCode/saveLocation.py
+++ server/pyCode/saveLocation.py
@@ -41,6 +41,11 @@ async def run():
     print("-- Setting initial setpoint")
     await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))
     
+    print("-- Arming")
+    await drone.action.arm()
+
+    await asyncio.sleep(5)
+
     print("-- Starting offboard")
     try:
         await drone.offboard.start()
@@ -55,19 +60,26 @@ async def run():
     await drone.offboard.set_position_ned(
             PositionNedYaw(0.0, 0.0, -5.0, 0.0))
     
+    await asyncio.sleep(10)
+
     position = await get_position(drone)
     x = position.latitude_deg
     y = position.longitude_deg
     z = position.relative_altitude_m
 
-    await asyncio.sleep(15)
-  
+    print("-- return home\
+          #1 Set the original coordinate to 0 ") # 아예 좌표를 0으로 처리
+    await drone.offboard.set_position_ned(
+            PositionNedYaw(0.0, 0.0, 0.0, 0.0))
+    await asyncio.sleep(10)
 
-    print("-- Arming")
-    await drone.action.arm()
-
-    await asyncio.sleep(5)
-    
+    print("-- Stopping offboard")
+    try:
+        await drone.offboard.stop()
+    except OffboardError as error:
+        print(f"Stopping offboard mode failed \
+                with error code: {error._result.result}")
+           
     print("-- Disarming the drone")
     await drone.action.disarm()
 
