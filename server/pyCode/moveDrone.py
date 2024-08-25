e690584550ebbad5331c4bac821a5afaf51869f2
Jiwon Ryu <ryujiwon11@naver.com>
2024-08-25 21:40:04 +0900

    Update moveDrone.py

--- server/pyCode/moveDrone.py
+++ server/pyCode/moveDrone.py
@@ -17,6 +17,21 @@ async def get_z(drone):
     async for home in drone.telemetry.home():
         return home.absolute_altitude_m
     
+async def get_d_z(drone):
+    async for position in drone.telemetry.position():
+        return position.relative_altitude_m
+
+async def calculate_absolute_altitude(drone):
+    # Get the absolute altitude from home
+    absolute_z = await get_z(drone)
+
+     # Get the relative altitude
+    relative_z = await get_d_z(drone)
+    
+    # Calculate the absolute altitude
+    cal_z = absolute_z + relative_z
+    return cal_z
+    
 def printArgs(n1,n2,n3):
     print(n1)
     print(n2)
@@ -45,15 +60,16 @@ async def run():
     await drone.action.takeoff()
     await asyncio.sleep(10)
 
-   # await wait_until_altitude_reached(drone, target_altitude=1.0)
-    #await asyncio.sleep(5)
+    # await wait_until_altitude_reached(drone, target_altitude=1.0)
+    await asyncio.sleep(5)
 
     current_x, current_y = await get_xy(drone)
-    current_z = await get_z(drone)
+    current_ab_z = await get_z(drone)
+    current_re_z = await get_d_z(drone)
     
-    print(f"  이동 전 GPS: ({current_x}, {current_y}, {current_z})")
+    print(f"  이동 전 GPS: ({current_x}, {current_y}, home 절대: {current_ab_z}, pos 올라간고도 {current_re_z})")
     # 드론을 특정 글로벌 좌표로 이동
-    
+
     try:
         float_x = float(sys.argv[1])
         float_y = float(sys.argv[2])
@@ -61,18 +77,21 @@ async def run():
     except ValueError:
         print("올바른 숫자 형식이 아닙니다.")
 
-    print(f" 서버로부터 받은 좌표 ({float_x}, {float_y}, {float_z})")
+    print(f"  서버로부터 받은 좌표 / {float_x}, {float_y}, {float_z } )")
     
     await drone.action.goto_location(float_x, float_y, float_z, 0)
-    await asyncio.sleep(15)
+    await asyncio.sleep(10)
     
-    #await wait_until_reached_global_position(drone, float_x, float_y, float_z)
-    #await asyncio.sleep(5)
+    """await wait_until_reached_global_position(drone, current_x, current_y, t)
+    await asyncio.sleep(5)"""
 
     current_x, current_y = await get_xy(drone)
-    current_z = await get_z(drone)
+    current_ab_z = await get_z(drone)
+    current_re_z = await get_d_z(drone)
+    current_total_z = await calculate_absolute_altitude(drone)
 
-    print(f"  이동 후 GPS: ({current_x}, {current_y}, {current_z})")
+    print(f"  이동 후 GPS: ({current_x}, {current_y}, {current_total_z})")
+    print(f"                home 절대: {current_ab_z}, pos 올라간고도 {current_re_z})")
 
     print("-- Landing")
     await drone.action.land()
