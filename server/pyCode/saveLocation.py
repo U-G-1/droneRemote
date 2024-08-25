671d8600c9378804afd5f6ac1e1b7f07d83f03d2
Jiwon Ryu <ryujiwon11@naver.com>
2024-08-25 21:34:20 +0900

    Update saveLocation.py

--- server/pyCode/saveLocation.py
+++ server/pyCode/saveLocation.py
@@ -16,6 +16,21 @@ async def get_z(drone):
     async for home in drone.telemetry.home():
         return home.absolute_altitude_m
     
+async def get_d_z(drone):
+    async for position in drone.telemetry.position():
+        return position.relative_altitude_m
+
+async def calculate_absolute_altitude(drone):
+    # Get the absolute altitude from home
+    absolute_z = await get_z(drone)
+
+     # Get the relative altitude
+    relative_z = await get_d_z(drone)
+    
+    # Calculate the absolute altitude
+    cal_z = absolute_z + relative_z
+    return cal_z
+    
 def printArgs(n1,n2,n3):
     print(n1)
     print(n2)
@@ -25,53 +40,74 @@ async def run():
     drone = System()
     await drone.connect(system_address="udp://:14540")
 
+    print("Waiting for drone to connect...")
     async for state in drone.core.connection_state():
         if state.is_connected:
-            flag = True
+            print("-- Connected to drone!")
             break
 
+    print("Waiting for drone to have a global position estimate...")
     async for health in drone.telemetry.health():
         if health.is_global_position_ok and health.is_home_position_ok:
-            flag = True
+            print("-- Global position state is good enough for flying.")
             break
 
+    print("-- Arming")
     await drone.action.arm()
     
+    print("-- Taking off")
     await drone.action.takeoff()
     await asyncio.sleep(10)
 
-    #await wait_until_altitude_reached(drone, target_altitude=1.0)
-    #await asyncio.sleep(5)
+    # await wait_until_altitude_reached(drone, target_altitude=1.0)
+    await asyncio.sleep(5)
 
     current_x, current_y = await get_xy(drone)
-    current_z = await get_z(drone)
+    current_ab_z = await get_z(drone)
+    current_re_z = await get_d_z(drone)
     
+    print(f"  이동 전 GPS: ({current_x}, {current_y}, home 절대: {current_ab_z}, pos 올라간고도 {get_d_z})")
     # 드론을 특정 글로벌 좌표로 이동
     
-    t_z = current_z + 8.0
+    t_z = current_ab_z + 5.0
+    print(f"  home 절대 + 올라간 고도 / {current_ab_z} + {current_re_z} ={t_z} )")
     
     await drone.action.goto_location(current_x, current_y, t_z, 0)
-    await asyncio.sleep(15)
+    await asyncio.sleep(10)
     
-    """await wait_until_reached_global_position(drone, current_x, current_y, current_z)
+    """await wait_until_reached_global_position(drone, current_x, current_y, t)
     await asyncio.sleep(5)"""
 
     current_x, current_y = await get_xy(drone)
-    current_z = await get_z(drone)
+    current_ab_z = await get_z(drone)
+    current_re_z = await get_d_z(drone)
+    current_total_z = await calculate_absolute_altitude(drone)
 
-    printArgs(current_x, current_y, current_z)
+    print(f"  이동 후 GPS: ({current_x}, {current_y}, {current_total_z})")
+    print(f"                home 절대: {current_ab_z}, pos 올라간고도 {get_d_z})")
 
+    printArgs(current_x, current_y, current_total_z)
+
+    print("-- Landing")
     await drone.action.land()
     await asyncio.sleep(15)
 
+    print("-- Disarming the drone")  #추가
     await drone.action.disarm()
 
+    print("Drone is disarmed and the script is done.")
+
 async def wait_until_altitude_reached(drone, target_altitude, tolerance=0.5):
-    async for home in drone.telemetry.home():
-        current_alt = home.absolute_altitude_m
+    """
+    드론이 목표 고도에 도달할 때까지 대기하는 함수
+    """
+    print("Waiting for the drone to reach the target altitude...")
+    async for position in drone.telemetry.position():
+        current_alt = position.relative_altitude_m
         alt_diff = abs(current_alt - target_altitude)
 
         if alt_diff <= tolerance:
+            print("Target altitude reached!")
             break
 
         await asyncio.sleep(1)  # 1초마다 고도 확인
@@ -93,6 +129,7 @@ async def wait_until_reached_global_position(drone, target_lat, target_lon, targ
         alt_diff = abs(current_alt - target_alt)
 
         if lat_diff <= tolerance and lon_diff <= tolerance and alt_diff <= tolerance:
+            print("Target position reached!")
             break
 
         await asyncio.sleep(0.5)  # 1초마다 위치 확인
