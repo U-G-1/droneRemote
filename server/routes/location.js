696db846fc2489d4351fa019b295af30eb5cfa28
Jiwon Ryu <ryujiwon11@naver.com>
2024-08-26 10:20:31 +0900

    land,save

--- server/routes/location.js
+++ server/routes/location.js
@@ -14,8 +14,10 @@ router.post('/execute', (req, res) => {
     const chimney = req.body.chimney;
     console.log('굴뚝 번호: ', chimney);
 
-    // 파이썬 스크립트 경로 설정
-    const scriptPath = path.join(__dirname, '..', 'pyCode', 'drone_test.py');
+
+    if(chimney==1){
+        // 파이썬 스크립트 경로 설정
+    const scriptPath = path.join(__dirname, '..', 'pyCode', 'drone_land_test.py');
 
     // 파이썬 스크립트를 spawn으로 실행 매개변수 : chimney
     const pythonProcess = spawn('python', [scriptPath, chimney]);
@@ -38,6 +40,33 @@ router.post('/execute', (req, res) => {
             res.status(500).send(`Script failed with code ${code}`);
         }
     });
+    }else if(chimney==2){
+        // 파이썬 스크립트 경로 설정
+    const scriptPath = path.join(__dirname, '..', 'pyCode', 'drone_save_test.py');
+
+    // 파이썬 스크립트를 spawn으로 실행 매개변수 : chimney
+    const pythonProcess = spawn('python', [scriptPath, chimney]);
+
+    // 스크립트 실행 중 표준 출력 처리
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    // 스크립트 실행 중 표준 오류 처리
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    // 스크립트 실행 종료 시 처리
+    pythonProcess.on('close', (code) => {
+        if (code === 0) {
+            res.send('Script executed successfully');
+        } else {
+            res.status(500).send(`Script failed with code ${code}`);
+        }
+    });
+    }
+    
 });
 
 module.exports = router;
