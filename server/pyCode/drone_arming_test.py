c9f8624605678c3fd5c87a083194a297f70df880
Jiwon Ryu <ryujiwon11@naver.com>
2024-08-27 15:37:53 +0900

    Update drone_arming_test.py

--- server/pyCode/drone_arming_test.py
+++ server/pyCode/drone_arming_test.py
@@ -17,17 +17,7 @@ async def run():
             print(f"Drone discovered!")
             break
             
-    #서버에서 이거 없애는 거 확인 필요
-    print("Waiting for drone to have a global position estimate...")
-    async for health in drone.telemetry.health():
-        if health.is_global_position_ok:
-            print("Global position estimate ok")
-            break
-    print("Fetching amsl altitude at home location....")
-    async for terrain_info in drone.telemetry.home():
-        absolute_altitude = terrain_info.absolute_altitude_m
-        break        
-
+    
     print("-- Arming")
     await drone.action.arm()
 
