910f8dee9ad59b89211092eed2e462e796a1132c
Jiwon Ryu <108641566+U-G-1@users.noreply.github.com>
2024-09-04 19:18:12 +0900

    moveDrone multipoint

--- server/models/location.js
+++ server/models/location.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { Model, DataTypes } = require("sequelize");
+const { Model, DataTypes, Op } = require("sequelize");
 
 module.exports = (sequelize) => {
   class Location extends Model {
@@ -34,13 +34,30 @@ module.exports = (sequelize) => {
       chim_name: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true,
+      },
+      chim_num: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
       },
       
     },
     {
       sequelize,
       modelName: "Location",
+      hooks: {
+        // beforeCreate 후크는 레코드가 생성되기 전에 실행됨
+        beforeCreate: async (location, options) => {
+          // 같은 chim_name을 가진 레코드 중 가장 높은 chim_num 값을 찾음
+          const maxChimNum = await Location.max('chim_num', {
+            where: {
+              chim_name: location.chim_name // 현재 입력하려는 chim_name과 동일한 레코드들 중에서 찾음
+            }
+          });
+
+          // 만약 maxChimNum 값이 존재하면 +1, 없으면(즉, 첫 레코드인 경우) 1로 설정
+          location.chim_num = maxChimNum !== null ? maxChimNum + 1 : 1;
+        },
+      },
     }
   );
 
--- server/routes/moveDrone.js
+++ server/routes/moveDrone.js
@@ -16,30 +16,44 @@ router.post('/move',async (req, res) => {
     const chimneyNumber = req.body.chimney; // 선택된 굴뚝 번호를 가져옵니다.
     const chimName = `굴뚝${chimneyNumber}`; // 선택된 굴뚝 번호에 해당하는 이름을 생성합니다.
 
-    // 데이터베이스에서 해당 chim_name을 가진 Location을 검색합니다.
-    const location = await Location.findOne({ where: { chim_name: chimName } });
+    // 데이터베이스에서 해당 chim_name을 가진 Location들을 검색하고 chim_num 오름차순으로 정렬합니다.
+    const locations = await Location.findAll({
+        where: { chim_name: chimName },
+        order: [['chim_num', 'ASC']],
+    });
 
     if (!location) {
         return res.status(404).send('Chimney not found');
     }
 
-    // 좌표 값을 변수에 저장합니다.
-    const x = location.loca_x;
-    const y = location.loca_y;
-    const z = location.loca_z;
-    const slope = location.slope;
+    // 검색된 Location들을 순회하며 x, y, z, slope 값을 추출하여 리스트를 만듭니다.
+    const coordinatesList = locations.map(location => [
+        location.loca_x,
+        location.loca_y,
+        location.loca_z,
+        // location.slope,
+    ]);
+    // 측정값 콘솔 출력
+    coordinatesList.forEach((coords, index) => {
+        console.log(`Chimney ${index + 1}: x = ${coords[0]}, y = ${coords[1]}, z = ${coords[2]}, slope = ${coords[3]}`);
+    });
 
-        // 측정값 콘솔
-        console.log('x :', x);
-        console.log('y :', y);
-        console.log('z :', z);
+    // // 좌표 값을 변수에 저장합니다.
+    // const x = location.loca_x;
+    // const y = location.loca_y;
+    // const z = location.loca_z;
+    // const slope = location.slope;
 
+    // // 측정값 콘솔
+    // console.log('x :', x);
+    // console.log('y :', y);
+    // console.log('z :', z);
 
     // 파이썬 스크립트 경로 설정
     const scriptPath = path.join(__dirname, '..', 'pyCode', 'moveDrone.py');
 
     // 파이썬 스크립트를 spawn으로 실행 매개변수 : chimney
-    const pythonProcess = spawn('python', ['-u',scriptPath, x,y,z,slope]);
+    const pythonProcess = spawn('python', ['-u',scriptPath, ...coordinatesList.flat()]);
 
     // 스크립트 실행 중 표준 출력 처리
     pythonProcess.stdout.on('data', (data) => {
