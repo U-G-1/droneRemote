d98cd4d35070249508229f53e813d1b9a94bc41a
Jiwon Ryu <ryujiwon11@naver.com>
2024-09-09 21:44:31 +0900

    Update mavDroneTest.py

--- server/pyCode/mavDroneTest.py
+++ server/pyCode/mavDroneTest.py
@@ -67,7 +67,7 @@ def main():
         print("-- 10s 소요: Landing")
         connection.mav.command_long_send(
             connection.target_system, connection.target_component,
-            mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0,
+            mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 0, 0, default_alt
         )
 
     land()
