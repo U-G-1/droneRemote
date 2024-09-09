911202e8257ed3dc1610919555232af671da9c80
Jiwon Ryu <ryujiwon11@naver.com>
2024-09-09 20:34:33 +0900

    Update mavDroneTest.py

--- server/pyCode/mavDroneTest.py
+++ server/pyCode/mavDroneTest.py
@@ -1,18 +1,19 @@
+
 import time
 from pymavlink import mavutil
 
 def main():
     # 연결 설정 (UDP 연결 예시)
     connection = mavutil.mavlink_connection('udp:127.0.0.1:14540')
+    #connection = mavutil.mavlink_connection('serial///dev/ttyUSB0:921600')
 
-    # Heartbeat 수신 대기 (드론과의 연결 확인)
+   # Heartbeat 수신 대기 (드론과의 연결 확인)
     print("Connecting to the drone...")
-    connection.wait_heartbeat(timeout=10)
+    connection.wait_heartbeat(10)
     if connection.target_system and connection.target_component:
         print("-- Connected to drone!")
     else:
         print("Failed to connect to the drone.")
-        return
 
     print("Waiting for drone to have a global position estimate...")
     msg = connection.recv_match(type='GPS_RAW_INT', blocking=True)
@@ -22,14 +23,17 @@ def main():
         if gps_status.fix_type >= 2:  # 2 = 2D fix, 3 = 3D fix
             print("-- Global position state is good enough for flying.")
             msg = connection.recv_match(type='GLOBAL_POSITION_INT', blocking=True)
-            if msg:
-                default_alt = msg.alt / 1000.0
+            default_alt = msg.alt / 1000.0
         else:
             print("Waiting for GPS data...")
 
-    # ARMING
+
+     # ARMING
     print("-- 3s 소요 Arming")
     connection.arducopter_arm()
+    """connection.mav.command_long_send(
+            connection.target_system, connection.target_component,
+            mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 1)"""
     time.sleep(3)
 
     # 이륙
@@ -37,7 +41,7 @@ def main():
         print("-- 10s 소요: Taking off")
         connection.mav.command_long_send(
             connection.target_system, connection.target_component,
-            mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, altitude
+            mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 10, 0, 0, 0, 0, 0, altitude
         )
 
     takeoff(10)
@@ -58,7 +62,7 @@ def main():
     connection.mav.command_long_send(
         connection.target_system,
         connection.target_component,
-        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 0
+        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,0
     )
 
 if __name__ == "__main__":
