49bb91e9239d77772acb3907ca83038743a93528
Jiwon Ryu <ryujiwon11@naver.com>
2024-09-09 16:22:16 +0900

    location

--- server/routes/location.js
+++ server/routes/location.js
@@ -28,6 +28,10 @@ router.post('/execute', (req, res) => {
         scriptPath = path.join(__dirname, '..', 'pyCode', 'drone_arming_test.py');
     }else if(chimney==4) {
         scriptPath = path.join(__dirname, '..', 'pyCode', 'mavDroneTest2.py');
+    }else if(chimney==5) {
+        scriptPath = path.join(__dirname, '..', 'pyCode', 'mavDroneLoca.py');
+    }else if(chimney==6) {
+        scriptPath = path.join(__dirname, '..', 'pyCode', 'mavDroneTest.py');
     }
 
     // 스크립트 경로가 설정되지 않은 경우 오류 반환
