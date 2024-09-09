29baaea480202a955885c7790f84a2607289de80
Jiwon Ryu <ryujiwon11@naver.com>
2024-09-09 21:24:47 +0900

    Update mavDroneTest.py

--- server/pyCode/mavDroneTest.py
+++ server/pyCode/mavDroneTest.py
@@ -1,6 +1,15 @@
 
 import time
+import threading
 from pymavlink import mavutil
+
+def send_heartbeat(master):
+        while True:
+            master.mav.heartbeat_send(
+                mavutil.mavlink.MAV_TYPE_GCS,  # Ground Control Station 타입
+                mavutil.mavlink.MAV_AUTOPILOT_INVALID, 
+                0, 0, 4)
+            time.sleep(1)  # 1초마다 heartbeat 전송
 
 def main():
     # 연결 설정 (UDP 연결 예시)
@@ -15,6 +24,11 @@ def main():
     else:
         print("Failed to connect to the drone.")
 
+     # Heartbeat 쓰레드 실행
+    heartbeat_thread = threading.Thread(target=send_heartbeat, args=(connection,))
+    heartbeat_thread.daemon = True  # 메인 프로그램 종료 시 쓰레드도 종료되도록 설정
+    heartbeat_thread.start()
+
     print("Waiting for drone to have a global position estimate...")
     msg = connection.recv_match(type='GPS_RAW_INT', blocking=True)
     if msg:
@@ -27,7 +41,7 @@ def main():
         else:
             print("Waiting for GPS data...")
 
-    time.sleep(10)
+    time.sleep(5)
 
      # ARMING
     print("-- 3s 소요 Arming")
@@ -35,7 +49,7 @@ def main():
     """connection.mav.command_long_send(
             connection.target_system, connection.target_component,
             mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 1)"""
-    time.sleep(10)
+    time.sleep(5)
 
     # 이륙
     def takeoff(altitude):
@@ -46,7 +60,7 @@ def main():
         )
 
     takeoff(10)
-    time.sleep(10)
+    time.sleep(5)
 
     # 착륙
     def land():
@@ -57,7 +71,7 @@ def main():
         )
 
     land()
-    time.sleep(10)
+    time.sleep(5)
     
     print("-- Disarm")
     connection.mav.command_long_send(
