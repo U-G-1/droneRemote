68a142083a87cc54c4043cf73317aa2db06b9487
tlswldls <sonia9.shin@gmail.com>
2024-09-12 16:16:58 +0900

    240912_sonia) Update drone command.py
    - 각 커맨드에서 명령 종료 후 모드 출력
    - 모드 변경 없이 특정 좌표로 이동하는 커맨드 추가 (20)

--- server/pyCode/drone_command.py
+++ server/pyCode/drone_command.py
@@ -6,6 +6,7 @@ Example of how to connect pymavlink to an autopilot via an UDP master
 # pylint: disable=W0702
 
 import time
+import sys
 # Import mavutil
 from pymavlink import mavutil
 
@@ -26,7 +27,7 @@ master.wait_heartbeat()
 
 # Get some information !
 while True:
-    command = int(input("enter menu\n1. get gps data\n2. arm\n3. takeoff\n4. land\n5. disarm\n"))
+    command = int(input("enter menu\n1. get gps data\n2. arm\n3. takeoff\n4. land\n5. disarm\n\n11. get mode\n12.change mode\n\n20. move drone\n----------\n"))
     
     if(command==1): # get gps
         try:
@@ -49,6 +50,14 @@ while True:
         print("Waiting for the vehicle to arm")
         master.motors_armed_wait()
         print('Armed!\n\n')
+        try:
+            hb = master.recv_match(type='HEARTBEAT', blocking=True)
+            print("base mode:", hb.base_mode,"\n")
+            print("custom mode:", hb.custom_mode,"\n")
+            print("mode:", mavutil.mode_string_v10(hb),"\n")
+        except:
+            print("get mode data failed")
+            pass
 
     elif(command==3): # takeoff
         master.mav.command_long_send(
@@ -56,7 +65,15 @@ while True:
             mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 5, 0, 0, 0, 37.5479590, 127.1197123, 10
         )
         print('Takeoff!\n\n')
-        time.sleep(10)
+        try:
+            hb = master.recv_match(type='HEARTBEAT', blocking=True)
+            print("base mode:", hb.base_mode,"\n")
+            print("custom mode:", hb.custom_mode,"\n")
+            print("mode:", mavutil.mode_string_v10(hb),"\n")
+        except:
+            print("get mode data failed")
+            pass
+        # time.sleep(10)
 
     elif(command==4): # land
         master.mav.command_long_send(
@@ -64,7 +81,15 @@ while True:
             mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 37.5479590, 127.1197123, 33
         )
         print('Land!\n\n')
-        time.sleep(10)
+        try:
+            hb = master.recv_match(type='HEARTBEAT', blocking=True)
+            print("base mode:", hb.base_mode,"\n")
+            print("custom mode:", hb.custom_mode,"\n")
+            print("mode:", mavutil.mode_string_v10(hb),"\n")
+        except:
+            print("get mode data failed")
+            pass
+        # time.sleep(10)
 
     elif(command==5): # disarm
         master.mav.command_long_send(
@@ -74,8 +99,85 @@ while True:
             0,
             0, 0, 0, 0, 0, 0, 0)
         print('Disarmed!\n\n')
+        try:
+            hb = master.recv_match(type='HEARTBEAT', blocking=True)
+            print("base mode:", hb.base_mode,"\n")
+            print("custom mode:", hb.custom_mode,"\n")
+            print("mode:", mavutil.mode_string_v10(hb),"\n")
+        except:
+            print("get mode data failed")
+            pass
 
         # wait until disarming confirmed
         print("Waiting for the vehicle to disarm")
         master.motors_disarmed_wait()
         print('Disarmed!\n\n')
+
+    elif(command==11): # get mode
+        try:
+            hb = master.recv_match(type='HEARTBEAT', blocking=True)
+            print("base mode:", hb.base_mode,"\n")
+            print("custom mode:", hb.custom_mode,"\n")
+            print("mode:", mavutil.mode_string_v10(hb),"\n")
+        except:
+            print("get mode data failed")
+            pass
+
+    elif(command == 12):  # Change  mode
+        """
+        usable mode
+        'MANUAL', 'STABILIZED', 'ACRO', 'RATTITUDE', 'ALTCTL', 'POSCTL', 'LOITER', 'MISSION', 'RTL', 'LAND', 'RTGS', 'FOLLOWME', 'OFFBOARD', 'TAKEOFF'
+        """
+        modes = ['MANUAL', 'STABILIZED', 'ACRO', 'RATTITUDE', 'ALTCTL', 'POSCTL', 'LOITER', 'MISSION', 'RTL', 'LAND', 'RTGS', 'FOLLOWME', 'OFFBOARD', 'TAKEOFF']
+        mode_num = int(input("enter mode what you want\n1. 'MANUAL'\n2. 'STABILIZED'\n3. 'ACRO'\n4. 'RATTITUDE'\n5. 'ALTCTL'\n6. 'POSCTL'\n7. 'LOITER'\n8. 'MISSION'\n9. 'RTL'\n10. 'LAND'\n11. 'RTGS'\n12. 'FOLLOWME'\n13. 'OFFBOARD'\n14. 'TAKEOFF'\n"))
+        if(mode_num>0 and mode_num<15):
+            mode = modes[mode_num-1]
+        else:
+            print("invalid number entered. try again")
+            break
+        # 모드가 유효한지 확인
+        if mode not in master.mode_mapping():
+            print('Unknown mode: {}'.format(mode))
+            print('Try:', list(master.mode_mapping().keys()))
+            exit(1)
+
+        # 모드 ID 가져오기 (튜플의 첫 번째 요소 사용)
+        mode_id = master.mode_mapping()[mode]
+
+        for _ in range(10):
+            # 비행 모드 전환 명령 전송
+            master.mav.command_long_send(
+                master.target_system,  # target_system
+                master.target_component,  # target_component
+                mavutil.mavlink.MAV_CMD_DO_SET_MODE,  # Command to set mode
+                0,  # Confirmation
+                mode_id[0],  # base_mode
+                mode_id[1],  # custom_mode (OFFBOARD mode ID)
+                mode_id[2], 
+                0, 0, 0, 0)  # Unused parameters
+
+            
+            print(f"Trying to switch to {mode} mode...")
+            ack_msg = master.recv_match(type='COMMAND_ACK', blocking=True)
+            ack_msg = ack_msg.to_dict()
+            if ack_msg['command'] == mavutil.mavlink.MAV_CMD_DO_SET_MODE:
+                if ack_msg['result'] == 0:  # Success
+                    print("Mode changed successfully.")
+                    break
+                else:
+                    print("Mode change failed, retrying...")
+            time.sleep(1)  # 1초 대기 후 재시도
+            
+    elif(command==20): #move drone
+        # GPS 좌표 설정 (목표 위도, 경도, 고도)
+        # 37.5479590, 127.1197123
+        target_lat = 37.5478690  # 위도 (10^-7 단위)
+        target_lon = 127.1197123  # 경도 (10^-7 단위)
+        target_alt = 10  # 고도 (미터, AMSL)
+
+        # 비행 모드 전환 명령 전송
+        master.mav.command_long_send(
+            master.target_system,  # target_system
+            master.target_component,  # target_component
+            mavutil.mavlink.MAV_CMD_DO_REPOSITION,  # Command to set mode
+            0, 0, 0, 0, 0, target_lat, target_lon, target_alt)  # Unused parameters
